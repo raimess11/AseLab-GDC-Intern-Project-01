e5811ced379d021c3ea75479e3651c5cb3eb9bbf
DzakyRaynaldy <110772319+DzakyRaynaldy@users.noreply.github.com>
2022-08-08 22:53:57 +0700

    Basic_movement_Player_True
    Basic movement player fix

---
+++ actor/Player/Player.gd
@@ -0,0 +1,24 @@
+extends KinematicBody2D
+
+var velocity = Vector2.ZERO
+var speed = 50
+var max_speed = 200
+var friction = 30
+
+
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+	
+	if input_vector != Vector2.ZERO:
+		velocity += input_vector * speed * delta
+		velocity = velocity.clamped(max_speed * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO,friction*delta)
+		
+		
+		
+	
+	move_and_collide(velocity)
