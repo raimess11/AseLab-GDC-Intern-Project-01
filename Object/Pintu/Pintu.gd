cc20c736a1013d75c85218fbdbfa9038e138655d
AdamRFaqih <adamrafif@student.telkomuniversity.ac.id>
2022-08-08 17:07:35 +0700

    Add open and close
    pintu tidak lagi sekali pakai dan tidak ada delay sampai animasi menutup dikeluarkan

--- Object/Pintu/Pintu.gd
+++ Object/Pintu/Pintu.gd
@@ -3,4 +3,8 @@ extends StaticBody2D
 func _on_Area2D_body_entered(_body):
 	$AnimationPlayer.play("Buka")
 	yield($AnimationPlayer,"animation_finished")
-	queue_free()
+	$Area2D.disabled = true
+	yield(get_tree().create_timer(5.0), "timeout")
+	$AnimationPlayer.play_backwards("Buka")
+	yield($AnimationPlayer,"animation_finished")
+	$Area2D.disabled = false
