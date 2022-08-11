a5caeebdd9009d544172dda3f61e2a0af2125593
AdamRFaqih <adamrafif@student.telkomuniversity.ac.id>
2022-08-11 20:04:35 +0700

    Add comment

--- Object/Pintu/Pintu.gd
+++ Object/Pintu/Pintu.gd
@@ -1,10 +1,11 @@
 extends StaticBody2D
 
+#Fungsi membuka dan menutup pintu
 func _on_Area2D_body_entered(_body):
-	$AnimationPlayer.play("Buka")
+	$AnimationPlayer.play("Buka") #Jalan animasi buka
 	yield($AnimationPlayer,"animation_finished")
-	$Area2D.disabled = true
-	yield(get_tree().create_timer(5.0), "timeout")
-	$AnimationPlayer.play_backwards("Buka")
+	$Area2D.disabled = true #Disable collision
+	yield(get_tree().create_timer(5.0), "timeout") #Delay pintu kebuka
+	$AnimationPlayer.play_backwards("Buka") #Animasi tutup
 	yield($AnimationPlayer,"animation_finished")
-	$Area2D.disabled = false
+	$Area2D.disabled = false #Enable collision
