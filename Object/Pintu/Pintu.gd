38a809a704e2d04539d558ea2ba61f3127002c78
AdamRFaqih <adamrafif@student.telkomuniversity.ac.id>
2022-08-13 06:16:57 +0700

    Fix pintu
    Menambahkan interaksi dengan player dan menambahkan animasi yang lebih baiik

--- Object/Pintu/Pintu.gd
+++ Object/Pintu/Pintu.gd
@@ -1,10 +1,20 @@
 extends StaticBody2D
 
-func _on_Area2D_body_entered(_body):
-	$AnimationPlayer.play("Buka")
+#ngasi tau node ini bisa interaksi
+func interaction_can_interact(interactionComponentParent : Node) -> bool :
+	return interactionComponentParent is Player
+
+#UI text yang mau dimunculin kalo masuk radius interaksi
+func interaction_get_text() -> String:
+	return "Press E to Open Door"
+
+#yang dilakuin pas interaksi
+# warning-ignore:unused_argument
+func interaction_interact(interactionComponentParent : Node) -> void :
+	$AnimationPlayer.play("Open") #Animasi Buka
 	yield($AnimationPlayer,"animation_finished")
-	$Area2D.disabled = true
-	yield(get_tree().create_timer(5.0), "timeout")
-	$AnimationPlayer.play_backwards("Buka")
+	$Area2D.disabled = true #Disable Collision
+	yield(get_tree().create_timer(5.0), "timeout") #Jeda Kebuka
+	$AnimationPlayer.play_backwards("Open") #Animasi Tutup
 	yield($AnimationPlayer,"animation_finished")
-	$Area2D.disabled = false
+	$Area2D.disabled = false #Enable Collision
