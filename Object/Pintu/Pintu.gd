afa5f602cd20d50445c9892dd3117273f4ad7bed
AdamRFaqih <adamrafif@student.telkomuniversity.ac.id>
2022-08-13 10:22:05 +0700

    Revisi pintu
    Bisa interaksi buka tutup

--- Object/Pintu/Pintu.gd
+++ Object/Pintu/Pintu.gd
@@ -1,10 +1,26 @@
 extends StaticBody2D
 
-func _on_Area2D_body_entered(_body):
-	$AnimationPlayer.play("Buka")
-	yield($AnimationPlayer,"animation_finished")
-	$Area2D.disabled = true
-	yield(get_tree().create_timer(5.0), "timeout")
-	$AnimationPlayer.play_backwards("Buka")
-	yield($AnimationPlayer,"animation_finished")
-	$Area2D.disabled = false
+#status pintu (false=tutup/true=buka)
+var status = false
+
+#ngasi tau node ini bisa interaksi
+func interaction_can_interact(interactionComponentParent : Node) -> bool :
+	return interactionComponentParent is Player
+
+#UI text yang mau dimunculin kalo masuk radius interaksi
+func interaction_get_text() -> String:
+	return "Press E to Open or Close Door"
+
+#yang dilakuin pas interaksi
+func interaction_interact(interactionComponentParent : Node) -> void :
+	#cek status pintu
+	if status == false:
+		#jalanin animasi
+		$AnimationPlayer.play("open")
+		yield($AnimationPlayer,"animation_finished")
+		#ubah status pintu jadi true(kebuka)
+		status = true
+	else:
+		$AnimationPlayer.play_backwards("open")
+		yield($AnimationPlayer,"animation_finished")
+		status = false
