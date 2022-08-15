574e2d41f43ed8dd6b5b206f2405661eba6b7f9c
mraihanfauzii <muhammad.raihan.fauzi@gmail.com>
2022-08-16 01:20:47 +0700

    UIUX/PopUpText & Pause Menu
    Pause menu udah dimasukin ke gamenya pas gamenya mulai pencet escape bakal ke pause ada pilihan resume atau main menu, kalau popuptext templatenya udah jadi, kalau perlu nanti tinggal dipanggil sama diisi textnya di gdnya

--- UIUX/pause_menu/PauseMenu.gd
+++ UIUX/pause_menu/PauseMenu.gd
@@ -1,16 +1,21 @@
-extends MarginContainer
+extends Control
 
 onready var selector_one = $CenterContainer/VBoxContainer/Selection/VBoxContainer/CenterContainer/HBoxContainer/Selector
 onready var selector_two = $CenterContainer/VBoxContainer/Selection/VBoxContainer/CenterContainer2/HBoxContainer/Selector
-onready var selector_three = $CenterContainer/VBoxContainer/Selection/VBoxContainer/CenterContainer3/HBoxContainer/Selector
 
 var current_selection = 0
 
 func _ready():
+	set_visible(false)
 	set_current_selection(0)
-	
+
+func _input(event):
+	if event.is_action_pressed("pause"):
+		set_visible(!get_tree().paused)
+		get_tree().paused = !get_tree().paused
+		
 func _process(delta):
-	if Input.is_action_just_pressed("ui_down") and current_selection < 2:
+	if Input.is_action_just_pressed("ui_down") and current_selection < 1:
 		current_selection +=1
 		set_current_selection(current_selection)
 	elif Input.is_action_just_pressed("ui_up") and current_selection > 0:
@@ -18,28 +23,20 @@ func _process(delta):
 		set_current_selection(current_selection)
 	elif Input.is_action_just_pressed("ui_accept"):
 		handle_selection(current_selection)
-
-#nanti kalo udah ada levelnya get_tree().change_scene("res://World.tscn") diubah
-#ke scene levelnya jadi nanti di main menu kalo klik start sama tutorial scenenya beda
-#sementara sama dulu ke World.tscn
+		
 func handle_selection(_current_selection):
 	if _current_selection == 0:
-		get_tree().change_scene("res://World.tscn")
-		queue_free()
+		get_tree().paused = false
+		set_visible(false)
 	elif _current_selection == 1:
-		get_tree().change_scene("res://World.tscn")
+		get_tree().change_scene("res://UIUX/main_menu/Main Menu.tscn")
+		get_tree().paused = false
 		queue_free()
-	elif _current_selection == 2:
-		get_tree().quit()
 
 func set_current_selection(_current_selection):
 	selector_one.text = ""
 	selector_two.text = ""
-	selector_three.text = ""
 	if _current_selection == 0:
 		selector_one.text = ">"
 	elif _current_selection == 1:
 		selector_two.text = ">"
-	elif _current_selection == 2:
-		selector_three.text = ">"
-		
