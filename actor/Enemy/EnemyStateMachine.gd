f754f50e56231944b7c2eff5e35bc5d352e77023
fadliz <asama.fadli@gmail.com>
2022-08-15 13:42:09 +0700

    Attack Indicator / Enemy announcement to attack
    simple indicator using sprite animation player, being played before an attack is done. Still needs polishing (AKA where the actual attack script???). Any better way found can tell me. better original vfx can ask GA.

--- actor/Enemy/EnemyStateMachine.gd
+++ actor/Enemy/EnemyStateMachine.gd
@@ -18,8 +18,12 @@ var velocity = Vector2.ZERO
 #Asumsi belum ketemu player
 var player = null
 
+#ambil node anim player anim indikatornya
+onready var attack_ind = $indicator.get_node("Attack").get_node("AnimationPlayer")
+onready var warning_ind = $indicator.get_node("Warning").get_node("AnimationPlayer")
 onready var start_position = global_position
 onready var target_position = global_position
+onready var bullet_scene = preload("res://Object/Bullet.tscn")
 
 func update_target_position():
 	var target_vector = Vector2(rand_range(-32, 32), rand_range(-32, 32))
@@ -47,6 +51,8 @@ func _ready():
 	state_enemy = STATE.IDLE # Replace with function body.
 	update_target_position()
 	$Timer.one_shot = true
+	$indicator/Attack.hide()
+	$indicator/Warning.hide()
 
 #Aksi Chase
 func _on_Area2D_body_entered(body): # Replace with function body.
@@ -59,6 +65,18 @@ func _on_Area2D_body_exited(body): # Replace with function body.
 	if "Player" in body.name:
 		player = null
 		state_enemy = STATE.WANDER
+
+func fire() :
+	match state_enemy :
+		STATE.CHASE :
+			#ambil peluru dari scene, set posisi dan player var
+			var bullet = bullet_scene.instance()
+			bullet.position = get_global_position()
+			bullet.player = player
+			#tambahin peluru yg baru dibuat ke child
+			get_parent().add_child(bullet)
+	#timer 5 detik
+	$BulletTimer.set_wait_time(5)
 
 func _physics_process(delta):
 	#Untuk Enemy
@@ -74,7 +92,6 @@ func _physics_process(delta):
 		STATE.CHASE:
 			if not (player == null):
 				velocity = position.direction_to(player.global_position) * run_speed * 100 * delta
-				
 		#Lakukan aksi Chase
 		STATE.WANDER:
 			accelerate_to_point(target_position, akselerasi * delta)
@@ -82,3 +99,22 @@ func _physics_process(delta):
 				state_enemy = STATE.IDLE
 				enemy_idle()
 	velocity = move_and_slide(velocity, Vector2.UP)
+
+func _on_BulletTimer_timeout():
+	#reset animation ke 0 detik
+	warning_ind.seek(0.2,true)
+	attack_ind.seek(0.2,true)
+	#play dan show animation 
+	warning_ind.play("warning")
+	$indicator/Warning.show()
+	#kalau animation selesai, yield sinyal selesai trus hide
+	yield(warning_ind,"animation_finished")
+	$indicator/Warning.hide()
+	#timer kalau perlu
+	yield(get_tree().create_timer(0.3), "timeout")
+	#hal yg sm dengan yg atas
+	attack_ind.play("attack")
+	$indicator/Attack.show()
+	yield(attack_ind,"animation_finished")
+	$indicator/Attack.hide()
+	fire()
