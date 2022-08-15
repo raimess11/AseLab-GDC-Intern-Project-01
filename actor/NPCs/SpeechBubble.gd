939cd9d3f04cb5c09247bf58b00543c054e0a646
Fakhrul0121 <106474317+Fakhrul0121@users.noreply.github.com>
2022-08-15 19:59:09 +0700

    testing pull req again

--- actor/NPCs/SpeechBubble.gd
+++ actor/NPCs/SpeechBubble.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 onready var dialogText = $Anchor/Chat
-onready var container = $Anchor/Container
+onready var container = $Anchor/Chat/Container
 onready var timer = $Timer
 onready var tween = $Tween
 onready var anchor = $Anchor
@@ -21,34 +21,12 @@ func showLine(dialogLine, lineNum):
 	assert(dialogLine, "dialog not found.")
 	#backend stuff and sizing
 	var textRaw = dialogLine[lineNum]["Chat"]
-	dialogText.bbcode_text = textRaw
-	#memisahkan dialog sebagai kumpulan line setiap containerLengthPerTextSize
-	var textAfterWhile = ""
-	var m = -1
-	var yTextSize = dialogText.get_font("normal_font").get_string_size(dialogText.text).y
-	var xTextSize = 0
-	var textSizeInLoop = 0
-	for x in range(0, len(textRaw)):
-		textAfterWhile += dialogText.text[x]
-		textSizeInLoop += dialogText.get_font("normal_font").get_string_size(dialogText.text[x]).x 
-		if (int(textSizeInLoop) / containerLengthPerTextSize) > 0:
-			if xTextSize < textSizeInLoop: xTextSize = textSizeInLoop
-			textSizeInLoop -= containerLengthPerTextSize
-			yTextSize += yTextSize
-			while textAfterWhile[m] == ' ':
-				textAfterWhile[m] = "\n"
-				m += -1
-	if yTextSize == dialogText.get_font("normal_font").get_string_size(dialogText.text).y:
-		xTextSize = textSizeInLoop
+	dialogText.text = textRaw
 	#atur margin/size container dan chat
-	dialogText.bbcode_text = textAfterWhile
-	dialogText.visible_characters = 0
-	container.margin_top = -yTextSize - marginOffset
-	dialogText.margin_top = -yTextSize - marginOffset
-	
-	container.margin_right = xTextSize + marginOffset
-	dialogText.margin_right = xTextSize + marginOffset
+	var xTextSize = containerLengthPerTextSize
+	dialogText.margin_right = xTextSize
 	#animation
+	dialogText.visible_characters = 0
 	tween.remove_all()
 	tween.interpolate_property(anchor, "position", Vector2(0,2), Vector2(0,0), 0.2)
 	tween.interpolate_property(container, "modulate", Color(1,1,1,0), Color(1,1,1,1), 0.2)
