3ee9c4d652a4c74e67f1087cd835f2a7eecf8961
Bhedil <aidilarifinnizar@gmail.com>
2022-08-20 13:09:49 +0700

    Cleaning the code
    - Menambahkan comment
    - merapihkan kodingan

--- actor/Enemy/State Machine V2.0 (masih revisi)/ProcessState.gd
+++ actor/Enemy/State Machine V2.0 (masih revisi)/ProcessState.gd
@@ -1,44 +1,38 @@
 extends KinematicBody2D
 
-#State apa saja yang terdapat dalam enemy/musuh
-enum STATE {
-	IDLE,
-	WANDER,
-	CHASE
-}
-
-#Atur kecepatan enemy
-var run_speed = 50
-
 #Variabel untuk kondisi state enemy
 var state_enemy
 
-#atur akselerasi enemy
-var akselerasi = 300
-
-# Atur toleransi jarak dengan player
-var tolerance = 4.0
-
-#Variabel untuk menggerakkan sprite
-var velocity = Vector2.ZERO
+#Memanggil script dari KecepatanMusuh.gd
+var move = load("res://KecepatanMusuh.gd").new()
 
 #Asumsi enemy belum ketemu player
 var player = null
 
-#Variabel untuk posisi awal 
-#Bernilai null
+#Memanggil variabel velocity dari KecepatanMusuh.gd
+var speedData = load("res://KecepatanMusuh.gd").new()
+var velocity = speedData.velocity
+
+#Memanggil variabel run_speed dari KecepatanMusuh.gd 
+var run = load("res://KecepatanMusuh.gd").new()
+var run_speed = run.run_speed
+
+#Memanggil variabel accel dari KecepatanMusuh.gd
+var akselerasi = load("res://KecepatanMusuh.gd").new()
+var accel = akselerasi.accel
+
+#Memanggil enumerator STATE dari State.gd
+const state = preload("res://State.gd")
+
+#Jarak toleransi antara enemy dengan player 
+var tolerance = 4.0
+
+#Variabel untuk posisi awal enemy
 onready var start_position = global_position
 
 #Variabel untuk mengetahui posisi target/player
-#Bernilai null
 #Digunakan pada state wander
-onready var target_position = global_position
-
-#Aksi enemy idling
-func _ready():
-	state_enemy = STATE.IDLE 
-	update_target_position()
-	$Timer.one_shot = true
+onready var target_position = global_position 
 
 #Mengubah posisi enemy secara random saat diluar jangkauan target
 func update_target_position():
@@ -52,55 +46,57 @@ func is_at_target_position():
 	# Akan berhenti saat target +/- toleransi
 	return (target_position - global_position).length() < tolerance
 
+#Fungsi untuk enemy berpikir memilih posisi secara random
+func enemy_idle():
+	$Timer.start(rand_range(0,3))
+	
 #Membuat enemy bergerak saat wander dalam posisi yang sudah ditetapkan (target_position)
 func accelerate_to_point(point, acceleration_scalar):
 	var direction = (point - global_position).normalized()
 	var acceleration_vector = direction * acceleration_scalar
 	accelerate(acceleration_vector)
 
-#Memungkinkan enemy untuk bergerak
-func accelerate(acceleration_vector):
-	velocity += acceleration_vector
-	velocity = velocity.clamped(run_speed)
-
-#Fungsi untuk enemy berpikir memilih posisi secara random
-func enemy_idle():
-	$Timer.start(rand_range(0,3))
+#Aksi enemy idling
+func _ready():
+	state_enemy = state.STATE.IDLE 
+	update_target_position()
+	$Timer.one_shot = true
 
 #Aksi enemy chasing player jika didalam jangkauan
 func _on_Area2D_body_entered(body): 
 	if "Player" in body.name:
 		player = body
-		state_enemy = STATE.CHASE
+		state_enemy = state.STATE.CHASE
 
 #Aksi enemy wandering jika player diluar jangkauan
 func _on_Area2D_body_exited(body): 
 	if "Player" in body.name:
 		player = null
-		state_enemy = STATE.WANDER
+		state_enemy = state.STATE.WANDER
 
 #Melakukan proses enemy
 func _physics_process(delta):
 	#Untuk Enemy
 	match state_enemy: 
 		#Lakukan aksi IDLE
-		STATE.IDLE:
+		state.STATE.IDLE:
 			velocity = Vector2.ZERO
 			#Transisi Enemy wandering
 			if $Timer.is_stopped():
-				state_enemy = STATE.WANDER
+				state_enemy = state.STATE.WANDER
 				update_target_position()
 			
 		#Lakukan aksi CHASE
-		STATE.CHASE:
+		state.STATE.CHASE:
 			if not (player == null):
 				velocity = position.direction_to(player.global_position) * run_speed * 100 * delta
 				
-		#Lakukan aksi WANDER
-		STATE.WANDER:
-			accelerate_to_point(target_position, akselerasi * delta)
+		#Lakukan aksi Wander
+		state.STATE.WANDER:
+			move.accelerate_to_point(target_position, accel * delta)
 			#Transisi Enemy idling
 			if is_at_target_position():
-				state_enemy = STATE.IDLE
+				state_enemy = state.STATE.IDLE
 				enemy_idle()
 	velocity = move_and_slide(velocity, Vector2.UP)
+
---
+++ actor/Enemy/State Machine V2.0 (masih revisi)/State.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+#State apa saja yang terdapat dalam enemy/musuh
+enum STATE {
+	IDLE,
+	WANDER,
+	CHASE
+}
+
+
+
+
+
