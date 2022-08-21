0cd043f6741c5a36c966b45ba175c2765b221f10
DzakyRaynaldy <110772319+DzakyRaynaldy@users.noreply.github.com>
2022-08-21 22:57:49 +0700

    Player_Attack
    menambahkan move attack player akan tetapi masih terdapat bug di mana player di bisa balik ke anim idle

--- actor/Player/Player.gd
+++ actor/Player/Player.gd
@@ -10,43 +10,51 @@ onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
 
 func _ready():
-	pass
+	animationTree.active = true
 var DASH = 50
 var dash_direction = -1
 var DASH_SPEED = 1.5 
 var is_cooldown = false
+var state = MOVE
 
 onready var timer = $Timer
 
+enum{
+	MOVE,
+	ATTACK,
+
+}
 
 
 
 func _physics_process(delta):
+	match state:
+		MOVE:
+			move_state(delta)
+		
+		ATTACK:
+			attack_state(delta)
+			
 	
+
+
+func _on_Timer_timeout():
+	is_cooldown = false
+
+func move_state(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
 	
-	if Input.get_action_strength("ui_right") : 
-		dash_direction = 1
-	elif Input.get_action_strength("ui_left") : 
-		dash_direction = -1
-	elif Input.get_action_strength("ui_up"):
-		dash_direction = -1
-	elif Input.get_action_strength("ui_down"):
-		dash_direction = 1
-	
-	
-	
-	
-	
-
+	if Input.is_action_just_pressed("attack"):
+			state = ATTACK
 	
 	if input_vector != Vector2.ZERO:
 		#animation
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Walk/blend_position", input_vector)
+		animationTree.set("parameters/Attack/blend_position", input_vector)
 		animationState.travel("Walk")
 		#thank you newton
 		velocity += input_vector * speed * delta
@@ -55,6 +63,17 @@ func _physics_process(delta):
 		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO,friction*delta)
 		
+		
+		
+	if Input.get_action_strength("ui_right") : 
+		dash_direction = 1
+	elif Input.get_action_strength("ui_left") : 
+		dash_direction = -1
+	elif Input.get_action_strength("ui_up"):
+		dash_direction = -1
+	elif Input.get_action_strength("ui_down"):
+		dash_direction = 1
+
 	if is_cooldown == false && Input.is_action_just_pressed("Dash") :
 		
 		if Input.is_action_pressed("ui_down")|| Input.is_action_pressed("ui_up"):
@@ -77,9 +96,12 @@ func _physics_process(delta):
 			velocity.x = DASH * 1/2 * sqrt(2) * DASH_SPEED
 			is_cooldown = true
 			timer.start(0)
-
-	
 	move_and_collide(velocity)
 
-func _on_Timer_timeout():
-	is_cooldown = false
+
+func attack_state(delta):
+	velocity = Vector2.ZERO
+	animationState.travel("Attack")
+
+func attack_finished():
+	state = MOVE
