61b1cdb1f695ca167ab230886910cc69b7f6197a
AdamRFaqih <adamrafif@student.telkomuniversity.ac.id>
2022-08-26 13:59:33 +0700

    Basic Pathfinding
    Belum work

--- actor/Enemy/EnemyStateMachine.gd
+++ actor/Enemy/EnemyStateMachine.gd
@@ -17,6 +17,8 @@ var tolerance = 4.0
 var velocity = Vector2.ZERO
 #Asumsi belum ketemu player
 var player = null
+var path: Array = []
+var levelNavigation: Navigation2D = null
 
 onready var start_position = global_position
 onready var target_position = global_position
@@ -44,8 +46,14 @@ func enemy_idle():
 	
 #Aksi IDLE
 func _ready():
+	yield(get_tree(), "idle_frame")
+	var tree = get_tree()
+	if tree.has_group("Navigasi"):
+		levelNavigation = tree.get_nodes_in_group("Navigasi")[0]
+	if tree.has_group("Player"):
+		player = tree.get_nodes_in_group("Player")[0]
 	state_enemy = STATE.IDLE # Replace with function body.
-	update_target_position()
+	#update_target_position()
 	$Timer.one_shot = true
 
 #Aksi Chase
@@ -53,12 +61,14 @@ func _on_Area2D_body_entered(body): # Replace with function body.
 	if "Player" in body.name:
 		player = body
 		state_enemy = STATE.CHASE
+		print("masuk")
 
 #Aksi WANDER
 func _on_Area2D_body_exited(body): # Replace with function body.
 	if "Player" in body.name:
 		player = null
 		state_enemy = STATE.WANDER
+		print("keluar")
 
 func _physics_process(delta):
 	#Untuk Enemy
@@ -70,15 +80,27 @@ func _physics_process(delta):
 				state_enemy = STATE.WANDER
 				update_target_position()
 			
-		#Lakukan aksi WANDER
+		#Lakukan aksi CHASE
 		STATE.CHASE:
-			if not (player == null):
-				velocity = position.direction_to(player.global_position) * run_speed * 100 * delta
-				
-		#Lakukan aksi Chase
+			if player and levelNavigation:
+				generate_path()
+				navigate()
+		#Lakukan aksi WANDER
 		STATE.WANDER:
 			accelerate_to_point(target_position, akselerasi * delta)
 			if is_at_target_position():
 				state_enemy = STATE.IDLE
 				enemy_idle()
 	velocity = move_and_slide(velocity, Vector2.UP)
+
+func navigate():    # Define the next position to go to
+	if path.size() > 0:
+		velocity = global_position.direction_to(path[1]) * run_speed
+		
+		# If reached the destination, remove this point from path array
+		if global_position == path[0]:
+			path.pop_front()
+
+func generate_path():
+	if levelNavigation != null and player != null:
+		path = levelNavigation.get_simple_path(global_position, player.global_position, false)
