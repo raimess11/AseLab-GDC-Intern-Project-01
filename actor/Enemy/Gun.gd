c9606a70604b3aa309576567f1ba507d0006fd8e
Fakhrul0121 <106474317+Fakhrul0121@users.noreply.github.com>
2022-09-07 10:24:37 +0700

    dodge (mostly) completed
    still very buggy

--- actor/Enemy/Gun.gd
+++ actor/Enemy/Gun.gd
@@ -3,18 +3,45 @@ extends Sprite
 var can_fire = true
 var bullet = preload("res://actor/Enemy/EnemyBullet.tscn")
 
+
+enum INDICATION_STATE{
+	black,
+	yellow,
+	red
+}
+
+#signal indikasi
+signal yellow(enemyName)
+signal red(bulletName)
+signal black(enemyName, bulletName)
+
+#untuk indikasi dodge player
+var indicationState = INDICATION_STATE.black
+var enemyAiming = false
+
 onready var player = get_node("/root/World/Player")
+onready var dodge = player.get_node("Dodge")
 onready var indicator = get_node("../AttackIndicator")
 onready var indicator_player = get_node("../AttackIndicator/IndicatorPlayer")
+
+#untuk nama bullet biar gambang dibedakan
+var timeStart: int = 0
+var timeNow: int = 0
+var bulletId: int = 0
 
 #init posisi
 func _ready():
 	set_as_toplevel(true)
 	position = get_parent().position
 	indicator.hide()
+	timeStart = OS.get_unix_time()
+
 
 
 func _physics_process(delta):
+#membuat id dari waktu
+	timeNow = OS.get_unix_time()
+	bulletId = timeNow - timeStart
 #lerp posisi biar muternya smooth
 	position.x = lerp(position.x, get_parent().position.x, 0.5)
 	position.y = lerp(position.y, get_parent().position.y, 0.5)
@@ -23,6 +50,7 @@ func _physics_process(delta):
 	
 	#cek jarak pistol sama player
 	if (global_position.distance_to(player.global_position) < 350) :
+		dodge.addEnemyList(get_parent().name, indicationState)
 		#suruh pistol liat player
 		look_at(player.global_position)
 		if (can_fire and $RayCast2D._collider_is_player()) :
@@ -35,11 +63,31 @@ func _physics_process(delta):
 				bullet_instance.rotation = rotation + rand_range(-0.1, 0.1)
 				bullet_instance.global_position = $Muzzle.global_position
 				indicator_player.play("Warning")
+				
 				yield(indicator_player, "animation_finished")
 				indicator_player.play("Attack")
+				indicationState = INDICATION_STATE.yellow
+				connect("yellow",dodge,"yellowTriggered")
+				emit_signal("yellow", get_parent().name)
 				yield(indicator_player, "animation_finished")
+				bullet_instance.name = "{0}bullet_{1}".format([bulletId, get_parent().name])
+				var bullet_name = bullet_instance.name
 				get_parent().add_child(bullet_instance)
+				get_parent().connect("child_exiting_tree", dodge, "bulletQueueFree")
+				connect("red",dodge,"redTriggered")
+				emit_signal("red", get_parent().get_node(bullet_name))
+				indicationState = INDICATION_STATE.red
 				can_fire = false
 				yield(get_tree().create_timer(1),"timeout")
+				indicationState = INDICATION_STATE.black
+				connect("black",dodge,"blackTriggered")
+				emit_signal("black",get_parent().name,bullet_name)
+				if get_parent().has_node("{0}bullet_{1}".format([bulletId, get_parent().name])):
+					bullet_instance.isUneffectedByDodge = true
 				can_fire = true
+	else:
+		enemyAiming = false
+		dodge.deleteEnemyList(get_parent().name)
+		
+		
 	
---
+++ actor/Player/Dodge.gd
@@ -0,0 +1,140 @@
+extends Node2D
+
+#list enemy yang lagi aim player beserta state indikasi
+var enemyList: Array = []
+var enemyNeedToBeDodged: Array = []
+var enemyListWithIndication: Dictionary = {}
+var bulletList: Array = []
+var listOfBulletNeedToBeDodged: Dictionary = {}
+var bullet
+
+#jika player gagal dodge
+var enemyDodgingFailed: Array = []
+var dodgingFailed = false
+
+#untuk chain attack
+var chainAttack = false
+var index = 0
+
+#timer dan cooldown
+onready var timer = $Timer
+onready var cooldown = $Cooldown
+var isCooldown = false
+
+enum INDICATION_STATE{
+	black,
+	yellow,
+	red
+}
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+func _process(delta):
+	bulletList = get_tree().get_nodes_in_group("bullets")
+	if dodgingFailed:
+		if enemyDodgingFailed == [] or enemyDodgingFailed[1] == INDICATION_STATE.black:
+			enemyDodgingFailed.clear()
+			dodgingFailed = false
+			return
+		else:
+			enemyDodgingFailed[1] = enemyListWithIndication[enemyDodgingFailed[0]]
+
+#add enemy pada list, panggil fungsi jika player di aim
+func addEnemyList(enemyName: String, indicatorState):
+	enemyListWithIndication[enemyName] = indicatorState
+	if !enemyList.has(enemyName):
+		enemyList.append(enemyName)
+	
+
+#delete enemy pada list, panggil jika enemy tidak aim player lagi
+func deleteEnemyList(enemyName: String):
+	enemyListWithIndication.erase(enemyName)
+	enemyList.erase(enemyName)
+
+func _input(event):
+	var x = enemyNeedToBeDodged.size() > 0
+	if Input.is_action_just_pressed("dodge") and x and !dodgingFailed:
+		dodging()
+	else:
+		print("failed")
+
+func yellowTriggered(enemyName):
+	enemyNeedToBeDodged.append(enemyName)
+
+func redTriggered(bullet):
+	listOfBulletNeedToBeDodged[bullet.name.split("_", true)[1]] = bullet
+
+func blackTriggered(enemyName, bulletName):
+	if enemyNeedToBeDodged.has(enemyName):
+		enemyNeedToBeDodged.erase(enemyName)
+	if listOfBulletNeedToBeDodged.has(enemyName):
+		listOfBulletNeedToBeDodged.erase(enemyName)
+
+func bulletQueueFree(node):
+	if node.name in bulletList:
+		if enemyNeedToBeDodged.has(node.name):
+			enemyNeedToBeDodged.erase(node.name)
+		if listOfBulletNeedToBeDodged.has(node.name):
+			listOfBulletNeedToBeDodged.erase(node.name)
+
+func dodging():
+	if !enemyListWithIndication.has(enemyNeedToBeDodged[0]):
+		endChainAttack()
+		return
+	if enemyListWithIndication[enemyNeedToBeDodged[0]] == INDICATION_STATE.yellow:
+		endChainAttack()
+		dodgingFailed = true
+	elif enemyListWithIndication[enemyNeedToBeDodged[0]] == INDICATION_STATE.red:
+		bullet = listOfBulletNeedToBeDodged[enemyNeedToBeDodged[0]]
+		var wr = weakref(bullet)
+		if (!wr.get_ref()):
+			print("bullet doesn't exist")
+			endChainAttack()
+			return
+		print(bullet)
+		bullet.get_node("Sprite").modulate = Color(1.0,1.0,1.0,1.0)
+		bullet.set_collision_mask_bit(1,false)
+		bullet.isDodged = true
+		bullet.isUneffectedByDodge = true
+		listOfBulletNeedToBeDodged.erase(enemyNeedToBeDodged[0])
+		enemyNeedToBeDodged.pop_at(0)
+		if enemyNeedToBeDodged.size() > 0:
+			if listOfBulletNeedToBeDodged.has(enemyNeedToBeDodged[0]):
+				bullet = listOfBulletNeedToBeDodged[enemyNeedToBeDodged[0]]
+			else:
+				endChainAttack()
+				return
+			wr = weakref(bullet)
+			if (!wr.get_ref()):
+				print("bullet doesn't exist")
+				endChainAttack()
+				return
+			print(bullet)
+			bullet.get_node("Sprite").modulate = Color(1.0,0.0,0.0,1.0)
+			if !chainAttack:
+				chainAttack = true
+				beginChainAttack()
+		else:
+			endChainAttack()
+
+func _on_Timer_timeout():
+	endChainAttack()
+
+func beginChainAttack():
+	Engine.time_scale = 0.01
+	timer.wait_time = 1.0 * Engine.time_scale
+	timer.start()
+
+func beginCooldown():
+	cooldown.wait_time = 1.0
+	cooldown.start()
+
+func endChainAttack():
+	index = 0
+	chainAttack = false
+	Engine.time_scale = 1.0
+	listOfBulletNeedToBeDodged.clear()
+	enemyNeedToBeDodged = []
