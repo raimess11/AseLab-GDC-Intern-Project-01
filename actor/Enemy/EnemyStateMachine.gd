c4afbd18641beaa6a9fece5350a73a9cfbad0e50
Bhedil <aidilarifinnizar@gmail.com>
2022-09-10 19:41:17 +0700

    Featuring enemy animation with new sprite sheet
    Import sprite sheet ke enemy dan Menambahkan animasi ke enemy

--- actor/Enemy/EnemyStateMachine.gd
+++ actor/Enemy/EnemyStateMachine.gd
@@ -7,6 +7,18 @@ enum STATE {
 	CHASE
 }
 
+#Variabel buat animasi enemy searah dengan player
+var facing = Vector2.ZERO
+
+#Manggil node animation tree
+onready var animationTree = get_node("AnimationTree")
+
+#Untuk implementasiin animasi
+onready var animationState = animationTree.get("parameters/playback")
+
+#Manggil node player
+onready var target = get_parent().get_node("Player")
+
 #Indeks posisi
 var indeksPost = 0
 
@@ -14,19 +26,19 @@ var indeksPost = 0
 export var posisiWander = [Vector2(100,100), Vector2(100, -50), Vector2(30, 30)]
 
 #Atur kecepatan enemy
-var run_speed = 50
+export var run_speed = 50
 
 #Variabel untuk kondisi state enemy
 var state_enemy
 
 #atur akselerasi enemy
-var akselerasi = 300
+export var akselerasi = 300
 
 # Atur toleransi jarak dengan player
-var tolerance = 4.0
+export var tolerance = 4.0
 
 #Variabel untuk menggerakkan sprite
-var velocity = Vector2.ZERO
+var velocity = Vector2.ZERO setget set_velocity 
 
 #Asumsi enemy belum ketemu player
 var player = null
@@ -44,6 +56,11 @@ onready var start_position = global_position
 #Digunakan pada state wander
 onready var target_position = global_position
 
+#Ngatur arah animasi enemy sesuai dengan arah player
+func set_velocity(value):
+	velocity = value
+	facing = velocity.normalized()
+
 #Aksi enemy idling
 func _ready():
 	state_enemy = STATE.IDLE 
@@ -51,8 +68,8 @@ func _ready():
 	$Timer.one_shot = true
 	if not has_gun :
 		$Gun.queue_free()
-
-#Mengubah posisi enemy secara random saat diluar jangkauan target
+		
+#Mengubah posisi enemy secara manual menggunakan array saat diluar jangkauan target
 func update_target_position():
 	#Inisialisasi posisi awal
 	var target_vector = posisiWander[indeksPost]
@@ -77,8 +94,8 @@ func accelerate_to_point(point, acceleration_scalar):
 
 #Memungkinkan enemy untuk bergerak
 func accelerate(acceleration_vector):
-	velocity += acceleration_vector
-	velocity = velocity.clamped(run_speed)
+	self.velocity += acceleration_vector
+	self.velocity = velocity.clamped(run_speed)
 
 #Fungsi untuk enemy berpikir memilih posisi secara random
 func enemy_idle():
@@ -89,6 +106,7 @@ func _on_Area2D_body_entered(body):
 	if "Player" in body.name:
 		player = body
 		state_enemy = STATE.CHASE
+		
 
 #Aksi enemy wandering jika player diluar jangkauan
 func _on_Area2D_body_exited(body): 
@@ -102,7 +120,9 @@ func _physics_process(delta):
 	match state_enemy: 
 		#Lakukan aksi IDLE
 		STATE.IDLE:
-			velocity = Vector2.ZERO
+			self.velocity = facing
+			animationState.travel("Idle")
+			set_anim_state()
 			#Transisi Enemy wandering
 			if $Timer.is_stopped():
 				state_enemy = STATE.WANDER
@@ -111,13 +131,37 @@ func _physics_process(delta):
 		#Lakukan aksi CHASE
 		STATE.CHASE:
 			if not (player == null):
-				velocity = position.direction_to(player.global_position) * run_speed * 100 * delta
+				animationState.travel("Walk")
+				set_anim_state()
+				#Enemy akan berhenti pada jarak tertentu dan siap mau nembak
+				if target.global_position.distance_to(global_position) < 100:
+					aim_still()
+					if velocity == Vector2.ZERO:
+						set_velocity(facing)
+						animationState.travel("Aim")
+						animationState.travel("Shoot")
+						set_anim_state()
+				else:
+					self.velocity = position.direction_to(player.global_position) * run_speed * 100 * delta
 				
 		#Lakukan aksi WANDER
 		STATE.WANDER:
 			accelerate_to_point(target_position, akselerasi * delta)
+			animationState.travel("Walk")
+			set_anim_state()
 			#Transisi Enemy idling
 			if is_at_target_position():
 				state_enemy = STATE.IDLE
 				enemy_idle()
-	velocity = move_and_slide(velocity, Vector2.UP)
+	self.velocity = move_and_slide(velocity, Vector2.UP)
+
+#Animasi enemy state	
+func set_anim_state():
+	animationTree.set("parameters/Walk/blend_position", facing)
+	animationTree.set("parameters/Shoot/blend_position", facing)
+	animationTree.set("parameters/Idle/blend_position", facing)
+	animationTree.set("parameters/Aim/blend_position", facing)
+
+#Untuk enemy berhentinya	
+func aim_still():
+	velocity = Vector2.ZERO
